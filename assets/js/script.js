6fae2dc71dbd3f0e80b43b38b57808b5f9a756c3
M Khaidar <62201903+haidar038@users.noreply.github.com>
2021-11-07 17:56:46 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function mode(x) {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    x.classList.toggle("bi-toggle-on");
+}
+
+function nav(mode) {
+    var navbar = document.getElementsByClassName("dark-mode-navbar");
+}
